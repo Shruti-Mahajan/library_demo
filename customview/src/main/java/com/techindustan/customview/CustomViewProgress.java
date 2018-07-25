02a9d4c05d412db00f9edf24df96595363314d46
shruti <shruti.techindustan@gmail.com>
2018-07-25 18:07:48 +0530

    dummy

---
+++ customview/src/main/java/com/techindustan/customview/CustomViewProgress.java
@@ -0,0 +1,13 @@
+package com.techindustan.customview;
+
+import android.util.Log;
+
+public class CustomViewProgress {
+
+    public static String tag = "abcedddd";
+
+    public static void showLog(String tag) {
+
+        Log.e(tag, tag);
+    }
+}
